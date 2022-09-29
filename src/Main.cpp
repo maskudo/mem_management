ebcc0f0fc73193a7832b196f1c9243a0e70807e2
maskudo <manujkarki101@gmail.com>
2022-09-29 22:42:53 +0545

    get block from previosly freed memory

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,44 @@
 #include<iostream>
+
+typedef char ALIGN[16];
+
+union header_t{
+    struct{
+        size_t size; //size of memory block 
+        bool is_free; //check if memory block is free
+        union header_t *next; //pointer to next header
+    }s;
+    ALIGN stub; //align the struct to 16 bytes 
+}
+header_t *head, *tail;
+
+void malloc(size_t size){
+   size_t total_size;
+   void *block;
+   header_t *header;
+   if (!size)
+        return NULL;
+   pthread_mutex_lock(&global_malloc_lock); //acquire lock before allocation memory to a thread
+   header = get_free_block(size);
+   if(header){
+        header->s.is_free = 0;
+        pthread_mutex_unlock(&global_malloc_lock); //give up lock after allocation
+        return *(void)(header + 1);
+   }
+       
+}
+//check  if there's a free block
+header_t *get_free_block(size_t size){
+    auto *curr = head;
+    while(curr){
+        if (curr->s.is_free && curr->s.size>=size){
+            return curr;
+        curr = curr->s.next;
+    }
+    return NULL;
+}
 int main(){
-    std::cout<<"hello world after editing lol"<<std::endl;
+    std::cout<<(void*)-1<<std::endl;
     return 0;
 }
 
