b604af266ed07f2703206d973deb261271e68a31
maskudo <manujkarki101@gmail.com>
2022-09-30 18:59:48 +0545

    finish malloc function

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,8 @@
 #include<iostream>
+#include<unistd.h>
+#include<stddef.h>
+
+pthread_mutex_t global_malloc_lock;
 
 typedef char ALIGN[16];
 
@@ -9,36 +13,58 @@ union header_t{
         union header_t *next; //pointer to next header
     }s;
     ALIGN stub; //align the struct to 16 bytes 
-}
-header_t *head, *tail;
+}*head, *tail;
 
-void malloc(size_t size){
+header_t *get_free_block(size_t size){
+    auto *curr = head;
+    while(curr){
+        if (curr->s.is_free && curr->s.size>=size)
+            return curr;
+        curr = curr->s.next;
+    }
+    return NULL;
+}   
+
+void *Malloc(size_t size){
    size_t total_size;
    void *block;
    header_t *header;
    if (!size)
         return NULL;
-   pthread_mutex_lock(&global_malloc_lock); //acquire lock before allocation memory to a thread
+   pthread_mutex_lock(&global_malloc_lock); //acquire lock before allocating memory to a thread
    header = get_free_block(size);
    if(header){
         header->s.is_free = 0;
         pthread_mutex_unlock(&global_malloc_lock); //give up lock after allocation
-        return *(void)(header + 1);
+        return (void*)(header + 1);
    }
-       
+   total_size = sizeof(header) + size;
+   block= sbrk(total_size);
+   if(block == (void*)-1){
+        pthread_mutex_unlock(&global_malloc_lock);
+        return NULL;
+   }
+   header = (header_t*)block;
+   header->s.size = size;
+   header->s.is_free = 0;
+   header->s.next = NULL;
+   if(!head){
+        head = header;
+   }
+   if (tail){
+        tail->s.next = header;
+   }
+   tail = header;
+   pthread_mutex_unlock(&global_malloc_lock);
+   return (void*)(header + 1);
 }
 //check  if there's a free block
-header_t *get_free_block(size_t size){
-    auto *curr = head;
-    while(curr){
-        if (curr->s.is_free && curr->s.size>=size){
-            return curr;
-        curr = curr->s.next;
-    }
-    return NULL;
-}
+
 int main(){
-    std::cout<<(void*)-1<<std::endl;
+    int *arr = (int*)Malloc(2*sizeof(int));
+    arr[0] = 1;
+    arr[1] = 2;
+    std::cout<<arr[0]<<std::endl<<arr[1];
     return 0;
 }
 
