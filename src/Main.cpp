390ab3fece0cbcb9dddef20bf06c2983efc8ea03
maskudo <manujkarki101@gmail.com>
2022-10-01 15:57:50 +0545

    add mem_free function

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <pthread.h>
 #include<unistd.h>
 #include<stddef.h>
 
@@ -38,7 +39,7 @@ void *Malloc(size_t size){
         pthread_mutex_unlock(&global_malloc_lock); //give up lock after allocation
         return (void*)(header + 1);
    }
-   total_size = sizeof(header) + size;
+   total_size = sizeof(header_t) + size;
    block= sbrk(total_size);
    if(block == (void*)-1){
         pthread_mutex_unlock(&global_malloc_lock);
@@ -58,13 +59,42 @@ void *Malloc(size_t size){
    pthread_mutex_unlock(&global_malloc_lock);
    return (void*)(header + 1);
 }
-//check  if there's a free block
 
+void Free(void* block){
+    if (!block){
+        return;
+    }
+    pthread_mutex_lock(&global_malloc_lock);
+    header_t *header = (header_t*)block -1;
+    auto cur_break = sbrk(0);
+    if (header->s.size + (char*)block == cur_break){
+        if (head == tail)
+            head = tail = NULL;
+        else{
+            auto temp = head;
+            while(temp){
+                if(temp->s.next == tail){
+                    temp->s.next = NULL;
+                    tail = temp;
+                }
+                temp = temp->s.next;
+            }
+        }
+        sbrk(0 - header->s.size + sizeof(header_t));
+        pthread_mutex_unlock(&global_malloc_lock);
+        std::cout<<"freed from end of the heap"<<std::endl;
+        return;
+    }
+    std::cout<<"freed"<<std::endl;
+    header->s.is_free = 1;
+    pthread_mutex_unlock(&global_malloc_lock);
+}
 int main(){
     int *arr = (int*)Malloc(2*sizeof(int));
     arr[0] = 1;
     arr[1] = 2;
-    std::cout<<arr[0]<<std::endl<<arr[1];
+    std::cout<<arr[0]<<std::endl<<arr[1]<<std::endl;
+    Free((void*)arr);
     return 0;
 }
 
